92cf64c06d7e702964c68b28b49c488d38c928c6
EdnaldoTaurino <ednaldo.taurino5@gmail.com>
2023-09-24 10:45:08 -0300

    adicionando exercicios

--- ED-lista1-questao28.c
+++ ED-lista1-questao28.c
@@ -10,6 +10,27 @@
 int main(){
 setlocale(LC_ALL, "Portuguese_BRazil");   
 
+
+    int hora1, minuto1, hora2, minuto2;
+    int diferencaHora, diferencaMinuto;
+
+    printf("Digite a primeira hora (hh:mm): ");
+    scanf("%d:%d", &hora1, &minuto1);
+
+    printf("Digite a segunda hora (hh:mm): ");
+    scanf("%d:%d", &hora2, &minuto2);
+
+    // Calcula a diferença em minutos
+    int totalMinutos1 = hora1 * 60 + minuto1;
+    int totalMinutos2 = hora2 * 60 + minuto2;
+    int diferencaTotalMinutos = totalMinutos2 - totalMinutos1;
+
+    // Calcula a diferença em horas e minutos
+    diferencaHora = diferencaTotalMinutos / 60;
+    diferencaMinuto = diferencaTotalMinutos % 60;
+
+    printf("A diferença entre as duas horas é: %02d:%02d\n", diferencaHora, diferencaMinuto);
+
     
 return 0;
 }
